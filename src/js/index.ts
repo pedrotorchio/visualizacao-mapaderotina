343531505519e81137c11e76a84d7d994caa55b6
Pedro Torchio <pt@WINDOWS.CIN.UFPE.BR>
2017-10-19 14:36:03 -0300

    tsconfig.allowjs

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,4 +1,14 @@
-import {Ready} from './components/ready';
+import D3Visualization from './components/D3Visualization';
+import App from './App';
+import $ from 'jquery';
 
-let ready = new Ready('Pedro');
-ready.greet();
+let APP = new App();
+APP.onPageReady(function(){
+  fakeuserinputevent();
+});
+
+function fakeuserinputevent(){
+  d3.json('/assets/tasks-joão.json', data=>{
+    APP.setData(data);
+  });
+}
